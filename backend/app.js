aba001a960df1ce1346ba523ae96f413b49f6cf6
Connor Burns <ctrekker4@gmail.com>
2020-08-30 11:01:47 -0600

    #17 Server use SSL connection if in production

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 if(env === 'production') {
-  console.log(path.join(__dirname, '../build'))
+  console.log(path.join(__dirname, '../build'));
   app.use(express.static(path.join(__dirname, '../build')));
 }
 app.use(express.static(path.join(__dirname, 'public')));
